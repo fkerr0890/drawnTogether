2e32da76fb60b44e68830a04f40f7ad1c9eff0c5
LunguRadu <lungu.radu123@gmail.com>
2021-11-18 18:56:09 -0600

    App with the Socket.io structure

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask
+from flask_socketio import SocketIO, send
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'mysecret'
+socketio = SocketIO(app, cors_allowed_origins='*')
 
+@socketio.on('message')
+def handleMessage(msg):
+	print('Message: ' + msg)
+	send(msg, broadcast=True)
 
-@app.route('/')
-def hello():
-    return render_template("index.html")
+if __name__ == '__main__':
+	socketio.run(app)
