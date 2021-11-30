1b8e9a83fd637b0be26cb8c8d33c4937175e4de4
LunguRadu <lungu.radu123@gmail.com>
2021-11-30 15:22:25 -0600

    added 2 teams

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ app.config['SECRET_KEY'] = 'mysecret'
 socketio = SocketIO(app, cors_allowed_origins='*')
 
 users = []
+team1 = []
+team2 = []
 
 
 @socketio.on('message')
@@ -16,7 +18,7 @@ def handleMessage(msg):
 
 @app.route("/")
 def draw():
-    return render_template("draw.html",users = users)
+    return render_template("draw.html", team1=team1, team2=team2)
 
 
 @app.route('/lobby')
@@ -29,7 +31,10 @@ def receive_username(username):
     print('Username: ' + username)
     send(username, broadcast=True)
     users.append({username: request.sid})
-    print(users)
+    if len(team1) <= len(team2):
+        team1.append(username)
+    else:
+        team2.append(username)
 
 
 if __name__ == '__main__':
