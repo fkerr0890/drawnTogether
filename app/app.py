f71044aae4994bbfdf1bdf33b38a2c0d06752cad
fkerr0890 <fredk101a@gmail.com>
2021-12-08 23:38:17 -0600

    Added new table to database for storing secret word and score data, new secret word is generated after correct guess, score now updates in UI, removed some weird words from bank although many remain

--- app/app.py
+++ app/app.py
@@ -3,7 +3,7 @@ import random
 
 import sqlalchemy.exc
 
-from flask import Flask, render_template, request, session
+from flask import Flask, render_template, request, session, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_socketio import SocketIO, send, join_room, leave_room, emit
 
@@ -24,6 +24,15 @@ class User(db.Model):
         return self.username
 
 
+class Data(db.Model):
+    __tablename__ = 'data'
+    type = db.Column(db.String(30), primary_key=True)
+    value = db.Column(db.String(30), nullable=False)
+
+    def __repr__(self):
+        return self.value
+
+
 @socketio.on('message')
 def handleMessage(msg):
     send(msg, broadcast=True)
@@ -33,21 +42,26 @@ def handleMessage(msg):
 def play(username):
     team1 = User.query.filter_by(team=0)
     team2 = User.query.filter_by(team=1)
+    team1_score = 0 if Data.query.filter_by(type='team0_score').first() is None else Data.query.filter_by(type='team0_score').first().value
+    team2_score = 0 if Data.query.filter_by(type='team1_score').first() is None else Data.query.filter_by(type='team1_score').first().value
     user = User.query.filter_by(username=username).first()
     try:
         word = get_random_noun()
-        db.session.add(User(id=word, username='word'))
+        db.session.add(Data(type='word', value=word))
         db.session.commit()
     except sqlalchemy.exc.IntegrityError:
         db.session.rollback()
-        word = User.query.filter_by(username='word').first().id
+        word = Data.query.filter_by(type='word').first().value
 
     return render_template("draw.html",
                            team1=team1,
                            team2=team2,
                            username=username,
                            drawer=user.drawer,
-                           word_to_guess=word)
+                           team=user.team,
+                           word_to_guess=word,
+                           team1_score=team1_score,
+                           team2_score=team2_score)
 
 
 @app.route('/')
@@ -96,10 +110,31 @@ def get_random_noun():
 
 
 @socketio.on('guess')
-def validate_guess(guess):
-    existing_word = User.query.filter_by(username='word').first().id
-    if guess.casefold() == existing_word.casefold():
+def validate_guess(msg):
+    msg = json.loads(msg)
+    existing_word = Data.query.filter_by(type='word').first().value
+    if msg.get('guess').casefold() == existing_word.casefold():
+        reset()
+        updateScore(msg.get('team'))
         send('game_over', broadcast=True)
+
+
+def updateScore(team):
+    data_type = 'team' + str(team) + '_score'
+    if Data.query.filter_by(type=data_type).first() is None:
+        db.session.add(Data(type=data_type, value="1"))
+        print(data_type + "1")
+    else:
+        existing_score = Data.query.get(data_type)
+        existing_score.value = str(int(existing_score.value) + 1)
+        print(data_type + existing_score.value)
+    db.session.commit()
+
+
+def reset():
+    word = Data.query.get('word')
+    db.session.delete(word)
+    db.session.commit()
 
 
 if __name__ == '__main__':
